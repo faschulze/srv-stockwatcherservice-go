2b27575e70dfb92408d38634526508ce2de826d8
Fabian Schulze <fabian.schulze@st.ovgu.de>
2022-10-02 22:33:28 +0200

    extended isOutsideOfBusinessHours func, add new test cases

--- utils/utils-helper.go
+++ utils/utils-helper.go
@@ -2,13 +2,20 @@ package utils
 
 import "time"
 
+const (
+	Saturday = "Saturday"
+	Sunday   = "Sunday"
+)
+
 // isOutsideOfBusinessHours func
 func (srv StockwatcherService) isOutsideOfBusinessHours(currentTime time.Time) bool {
 	weekday := currentTime.Weekday().String()
-	if weekday != "Saturday" && weekday != "Sunday" {
-		//if currentTime != sa && currentTime != so && currentTime >= 7:30 && cucurrentTime <= 22:30{
-		return false
+	if weekday != Saturday && weekday != Sunday {
+		openingHour := time.Date(currentTime.Year(), currentTime.Month(), currentTime.Day(), 7, 30, 0, 0, currentTime.Location())
+		closingHour := time.Date(currentTime.Year(), currentTime.Month(), currentTime.Day(), 22, 30, 0, 0, currentTime.Location())
+		if currentTime.After(openingHour) && currentTime.Before(closingHour) {
+			return false
+		}
 	}
-
 	return true
 }
--- utils/utils-helper_test.go
+++ utils/utils-helper_test.go
@@ -5,10 +5,26 @@ import (
 	"time"
 )
 
-func TestIsOutsideOfBusinessHours(t *testing.T) {
+func TestIsOutsideOfBusinessHours1(t *testing.T) {
 	srv := StockwatcherService{}
 	ti := time.Date(2022, 9, 29, 18, 30, 0, 0, time.Local)
 	if srv.isOutsideOfBusinessHours(ti) {
-		t.Fatal("test variable is outside of business hours, but should be within business hours")
+		t.Fatal("(1) test variable is outside of business hours, but should be within business hours")
+	}
+}
+
+func TestIsOutsideOfBusinessHours2(t *testing.T) {
+	srv := StockwatcherService{}
+	ti := time.Date(2022, 9, 29, 22, 35, 0, 0, time.Local)
+	if !srv.isOutsideOfBusinessHours(ti) {
+		t.Fatal("(2) test variable is inside of business hours, but should be outside of business hours")
+	}
+}
+
+func TestIsOutsideOfBusinessHours3(t *testing.T) {
+	srv := StockwatcherService{}
+	ti := time.Date(2022, 10, 1, 18, 30, 0, 0, time.Local)
+	if !srv.isOutsideOfBusinessHours(ti) {
+		t.Fatal("(3) test variable is inside of business hours, but should be outside of business hours")
 	}
 }
