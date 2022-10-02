ad33037fbe8ffb9cec9a95a5feb0cf89572a1cf0
Fabian Schulze <fabian.schulze@st.ovgu.de>
2022-10-02 21:17:37 +0200

    minor adjustment in isOutsideOfBusinessHours helper function

--- utils/utils-helper.go
+++ utils/utils-helper.go
@@ -4,8 +4,7 @@ import "time"
 
 // isOutsideOfBusinessHours func
 func (srv StockwatcherService) isOutsideOfBusinessHours(currentTime time.Time) bool {
-	dt := time.Now().Weekday()
-	weekday := dt.String()
+	weekday := currentTime.Weekday().String()
 	if weekday != "Saturday" && weekday != "Sunday" {
 		//if currentTime != sa && currentTime != so && currentTime >= 7:30 && cucurrentTime <= 22:30{
 		return false
